c123809a3a03530184ff66b1506785e77afcd25b
nirmit23 <nirmitshah231999@gmail.com>
2025-10-02 13:09:52 -0400

    npm run build npx next export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Tell Next.js we want static HTML export
     output: 'export',
+  
+    // Required for GitHub Pages (repo name is 123)
     basePath: '/123',
     assetPrefix: '/123/',
+  
+    images: {
+      unoptimized: true, // GitHub Pages doesn’t support Next.js image optimization
+    },
   };
   
   module.exports = nextConfig;
