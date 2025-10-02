aa5d6b685ce5ce970c41d2b58bc530173c278368
nirmit23 <nirmitshah231999@gmail.com>
2025-10-02 15:01:35 -0400

    eewede

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  basePath: '/123',   // repo name
+  basePath: '/123',   // 👈 repo name
   assetPrefix: isProd ? '/123/' : '',
 };
+
+module.exports = nextConfig;
