46989045cf92768a84046a5a5332d51f262447bf
nirmit23 <nirmitshah231999@gmail.com>
2025-10-02 13:08:23 -0400

    12345

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/123',       // must match your repo name
-    assetPrefix: '/123/',   // ensures assets load from the right path
-    trailingSlash: true,    // <-- important for GitHub Pages
+    basePath: '/123',
+    assetPrefix: '/123/',
   };
   
-  export default nextConfig;
+  module.exports = nextConfig;
   
